400bfbdab4254030dcd027d45bbb2936bf3ce9dd
jeyantharan <87352558+jeyantharan@users.noreply.github.com>
2025-09-10 01:06:35 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ function App() {
       }
 
       try {
-        const response = await axios.post('https://backend-5v9dozs75-jeys-projects-10abfd47.vercel.app/api/verifyToken', {
+        const response = await axios.post('https://backend-d1ri3v5qm-jeys-projects-10abfd47.vercel.app/api/verifyToken', {
           token: token
         });
         
